eb69b59d3ae75d5885bdc771e217f769dcdcab7e
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-18 19:55:29 -0400

    Added api-routes.js & html-routes.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const mongojs = require('mongojs');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(logger('dev'));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static("public"));
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(express.json());
-app.use(express.static('public'));
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
